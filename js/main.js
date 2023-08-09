bbda321db612cd7e8b7dd2cc0be2f49ab510fc50
Nick <54436939+Nickpatel008@users.noreply.github.com>
2023-08-09 22:56:52 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -152,8 +152,8 @@
 function myFunction(e) {
 	var x = e.clientX;
 	var y = e.clientY;
-	document.getElementById("tooltip").style.left = (x - 150) + "px";
-	document.getElementById("tooltip").style.top = (y - 180) + "px";
+	document.getElementById("tooltip").style.left = x + "px";
+	document.getElementById("tooltip").style.top = y + "px";
 }
 
 new kursor({
